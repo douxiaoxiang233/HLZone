c282b2018737e036c5a344ff71c776ec0e4194d8
douxiaoxiang233 <2051703348@qq.com>
2019-02-14 14:13:02 +0800

    0.0.3

--- app/src/main/java/com/XL/hlzone/MainActivity.java
+++ app/src/main/java/com/XL/hlzone/MainActivity.java
@@ -2,6 +2,7 @@ package com.XL.hlzone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import cn.bmob.v3.Bmob;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Bmob.initialize(this,"ba5f2c8f1d0a06efb63bef849e365fe1");
     }
 }
