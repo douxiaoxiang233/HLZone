502fa0d047a4b3c68127c9ea2871d3255fc7821a
lyn0904 <44257351+lyn0904@users.noreply.github.com>
2019-02-14 15:17:36 +0800

    Revert "0.0.3"
    This reverts commit c282b2018737e036c5a344ff71c776ec0e4194d8.

--- app/src/main/java/com/XL/hlzone/MainActivity.java
+++ app/src/main/java/com/XL/hlzone/MainActivity.java
@@ -2,7 +2,6 @@ package com.XL.hlzone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import cn.bmob.v3.Bmob;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Bmob.initialize(this,"ba5f2c8f1d0a06efb63bef849e365fe1");
+
     }
 }
